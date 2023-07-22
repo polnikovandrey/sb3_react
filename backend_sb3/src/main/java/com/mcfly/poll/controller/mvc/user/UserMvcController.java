ed467550fb07c700afb68a14dd02b056a36ce0d6
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-22 17:36:50 +0300

    + thymeleaf templates refactoring

--- backend_sb3/src/main/java/com/mcfly/poll/controller/mvc/user/UserMvcController.java
+++ backend_sb3/src/main/java/com/mcfly/poll/controller/mvc/user/UserMvcController.java
@@ -22,9 +22,15 @@ public class UserMvcController {
 
     @GetMapping({"/list", "/list/{page}"})
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public String users(@PathVariable(required = false) Optional<Integer> page, Model model) {
+    public String listUsers(@PathVariable(required = false) Optional<Integer> page, Model model) {
         final PagedResponse<UserResponse> users = userService.findAllUsers(page.orElse(0), 10);
         model.addAttribute("users", users);
-        return "users";
+        return "listUsers";
+    }
+
+    @GetMapping("/add")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String addUser() {
+        return "addUser";
     }
 }
