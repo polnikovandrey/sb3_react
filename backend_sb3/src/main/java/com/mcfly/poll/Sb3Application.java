e54e39c640c6863dddbe25e80156098b7b5e7344
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-28 16:17:53 +0300

    + UserServiceTest

--- backend_sb3/src/main/java/com/mcfly/poll/Sb3Application.java
+++ backend_sb3/src/main/java/com/mcfly/poll/Sb3Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 @SpringBootApplication
-// Register JPA 2.1 converters: convert Java 8 Date/Time fields to SQL upon persisting.
-//@EntityScan(basePackageClasses = {Sb3Application.class, Jsr310Converters.class})      TODO check wo
 public class Sb3Application {
 
     public static void main(String[] args) {
